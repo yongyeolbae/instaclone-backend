2a30edbebda2a9890a90a31865ee561dbc7ef0e0
yongyeolbae <mungnani@naver.com>
2022-01-25 06:41:09 +0900

    add node_modules

--- schema.js
+++ schema.js
@@ -2,8 +2,8 @@ import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
 
 
-const loadedTypes=loadFilesSync(`${__dirname}/**/*.typeDefs.js`,{ extensions: ['gql'] });
-const loadedResolvers=loadFilesSync(`${__dirname}/**/*.resolvers.js`,{ extensions: ['gql'] })
+const loadedTypes=loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers=loadFilesSync(`${__dirname}/**/*.resolvers.js`)
 export const typeDefs=mergeTypeDefs(loadedTypes);
 export const resolvers=mergeResolvers(loadedResolvers);
 
